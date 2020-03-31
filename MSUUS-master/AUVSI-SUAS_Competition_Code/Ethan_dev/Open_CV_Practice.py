5ebefc186a48129b802d66e989bb6c1dcf82fc8f
Ethan Clifford <ethanclifford99@gmail.com>
2020-03-31 16:49:58 -0400

    updated image filtering and cropping

--- MSUUS-master/AUVSI-SUAS_Competition_Code/Ethan_dev/Open_CV_Practice.py
+++ MSUUS-master/AUVSI-SUAS_Competition_Code/Ethan_dev/Open_CV_Practice.py
@@ -3,19 +3,20 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
+import math
 #test images
 #img = cv2.imread('image0 copy.jpg')
 #img = cv2.imread('smiley_grass_black.jpg')
 #img = cv2.imread('smiley_grass_white.jpg')
 #img = cv2.imread('smiley_grass.jpg')
 #img = cv2.imread('Grass_big_pentagon.jpg')
-img = cv2.imread('Grass_pentagon.jpg')
+#img = cv2.imread('Grass_pentagon.jpg')
 #img = cv2.imread('Grass1_2_red_square.jpg')
 #img = cv2.imread('InkedGrass1.jpg')
 #img = cv2.imread('test copy.jpeg')
 #img = cv2.imread('test_image copy.png')
 #img = cv2.imread('Grass_tiles.PNG')
-#img = cv2.imread('DSC00342.JPG')
+img = cv2.imread('DSC00342.JPG')
 #img = cv2.imread('DSC00343.JPG')
 #img = cv2.imread('DSC00344.JPG')
 #img = cv2.imread('DSC00345.JPG')
@@ -29,115 +30,52 @@ img = cv2.imread('Grass_pentagon.jpg')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 def crop_img(img,indx,done=False):
-
-    if done is False:
-        plt.imshow(img)
-        plt.show()
-        #sets the background of the image to black
-        mask = np.zeros(img.shape[:2],np.uint8)
-        bgdModel = np.zeros((1,65),np.float64)
-        fgdModel = np.zeros((1,65),np.float64)
-        rect = (50,50,450,290)
-        cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-        mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-        img = img*mask2[:,:,np.newaxis]
-        plt.imshow(img)
-        #plt.show()
-
-    #picks out a specific object in the image and crops out everything but that
-    #code from opencv's website on watershed
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    ret, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    # noise removal
-    kernel = np.ones((3, 3), np.uint8)
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-    # sure background area
-    sure_bg = cv2.dilate(opening, kernel, iterations=3)
-    # Finding sure foreground area
-    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-    ret, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(), 255, 0)
-    # Finding unknown region
-    sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg, sure_fg)
-    # Marker labelling
-    ret, markers = cv2.connectedComponents(sure_fg)
-    # Add one to all labels so that sure background is not 0, but 1
-    markers = markers+1
-    # Now, mark the region of unknown with zero
-    markers[unknown == 255] = 0
-    markers = cv2.watershed(img, markers)
-    img[markers == -1] = [255, 255, 255]
-    loc = []
     plt.imshow(img)
     plt.show()
-
-    #performs the cropping of the image
-    for i in range(len(markers)):
-        for j in range(len(markers[i])):
-            if markers[i][j] == 1:
-                loc.append((i, j))
-    if len(loc) > 1:
-        loc.pop(0)
-        x = []
-        for i in range(len(loc)):
-            x.append(loc[i][0])
-        y = []
-        for i in range(len(loc)):
-            y.append(loc[i][1])
-        minx = np.min(x)-3
-        miny = np.min(y)-3
-        maxx = np.max(x)+3
-        maxy = np.max(y)+3
-        img[markers == -1] = [255, 0, 0]
-        plt.imshow(img)
-        plt.show()
-        roi = img[minx:maxx, miny:maxy]
-        #roi_rgb = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-        plt.imshow(roi)
-        plt.show()
-        cv2.imwrite("roi.jpg",roi)
-"""
-    #splits image to look for more objects
-    i = 0
-    j = 0
-    x = x-minx
-    y = y-miny
-    xmin2 = np.min(x)
-    ymin2 = np.min(y)
-    xmax2 = xmin2
-    ymax2 = ymin2
-    while (x[i]-x[i+1]) < 4 and i < len(x)-2:
-        xmax2 = x[i]
-        i += 1
-    xmin2 = minx
-
-    while (y[j]-y[j+1]) < 4 and j < len(y)-2:
-        ymax2 = y[j]
-        j += 1
-    ymin2 = miny
-    print(y[j])
-    print(x[i])
-    print(xmin2)
-    print(xmax2)
-    print(ymin2)
-    print(ymax2)
-    img1 = roi[minx:xmax2, miny:ymax2]
-    cv2.imwrite("output1.jpg",img1)
-    plt.imshow(img1)
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    plt.imshow(hsv)
     plt.show()
-    k = xmin2
-    l = ymin2
-    #print(len(roi_rgb[0][:]))
-    #print(len(roi_rgb[:][0]))
-    while k < xmax2:
-        while l < ymax2:
-            #print(k,l)
-            roi[k,l] = [0,0,0]
-            l += 1
-        l = ymin2
-        k += 1
-    crop_img(roi,0,True)
-    """
+    mask = cv2.inRange(hsv, (0, 0, 0), (180, 255, 190))
+    ## slice the brown and dull
+    imask = mask <= 0
+    brown = np.zeros_like(hsv, np.uint8)
+    brown[imask] = hsv[imask]
+    plt.imshow(brown)
+    plt.show()
+
+    img = cv2.cvtColor(brown, cv2.COLOR_HSV2RGB)
+
+    # from https://stackoverflow.com/questions/37803903/opencv-and-python-for-auto-cropping
+    imgray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    imgray = cv2.blur(imgray, (15, 15))
+    ret, thresh = cv2.threshold(imgray, math.floor(np.average(imgray)), 255, cv2.THRESH_BINARY_INV)
+    dilated = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10)))
+    _, contours, _ = cv2.findContours(dilated, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    new_contours = []
+    for c in contours:
+        # if cv2.contourArea(c) < 960000:
+        if 100000 < cv2.contourArea(c) < 20000000:
+            new_contours.append(c)
+
+    best_box = [-1, -1, -1, -1]
+    for c in new_contours:
+        x, y, w, h = cv2.boundingRect(c)
+        if best_box[0] < 0:
+            best_box = [x, y, x + w, y + h]
+        else:
+            if x < best_box[0]:
+                best_box[0] = x - 5
+            if y < best_box[1]:
+                best_box[1] = y - 5
+            if x + w > best_box[2]:
+                best_box[2] = x + w + 5
+            if y + h > best_box[3]:
+                best_box[3] = y + h + 5
+
+    out = img[best_box[1]:best_box[3], best_box[0]:best_box[2]]
+    plt.imshow(out)
+    plt.show()
 
 
 crop_img(img,0)

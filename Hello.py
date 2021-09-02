152a618033a4ff02fd73b4bff80c24b95d2817f5
EthanClifford <ethanclifford99@gmail.com>
2021-09-01 21:44:29 -0400

    Add files via upload
    Added Hello.py, which is the code to run a simulated flight in mission planner

---
+++ Hello.py
@@ -0,0 +1,68 @@
+print("Start simulator (SITL)")
+import dronekit_sitl
+import time
+from pymavlink import mavutil
+sitl = dronekit_sitl.start_default()
+connection_string = sitl.connection_string()
+
+# Import DroneKit-Python
+from dronekit import connect, VehicleMode
+
+# Connect to the Vehicle.
+print("Connecting to vehicle on: %s" % (connection_string,))
+vehicle = connect(connection_string, wait_ready=True)
+
+# Get some vehicle attributes (state)
+print("Get some vehicle attribute values:")
+print (" GPS: %s" % vehicle.gps_0)
+print (" Battery: %s" % vehicle.battery)
+print (" Last Heartbeat: %s" % vehicle.last_heartbeat)
+print (" Is Armable?: %s" % vehicle.is_armable)
+print (" System status: %s" % vehicle.system_status.state)
+print (" Mode: %s" % vehicle.mode.name)    # settable
+
+
+def arm_and_takeoff(aTargetAltitude):
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Check that vehicle has reached takeoff altitude
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+# Initialize the takeoff sequence to 20m
+arm_and_takeoff(40)
+
+print("Take off complete")
+
+# Hover for 10 seconds
+time.sleep(10)
+
+print("Now let's land")
+vehicle.mode = VehicleMode("LAND")
+# Close vehicle object before exiting script
+vehicle.close()
+
+# Shut down simulator
+sitl.stop()
+print("Completed")
